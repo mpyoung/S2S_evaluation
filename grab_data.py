4555600b5facd9edd3dc17b2565847b97a093296
Matthew Young <myoung02@jasmin-sci5.ceda.ac.uk>
2018-10-17 17:29:20 +0100

    Added functions for reading 2mT from data

--- grab_data.py
+++ grab_data.py
@@ -219,7 +219,7 @@ def grab_t2m_region(inputfile,lonlim,latlim,dates):
   d1_id	= np.where(final_times[:,2]==dates[ 0,2])[0]
   d2_id	= np.where(final_times[:,2]==dates[-1,2])[0]
 
-  # always start accumlation from hour 6 (assuming this is the accumulation between 0z-6z)
+  # always start accumlation from hour 0 (assuming this is the accumulation between 0z-6z)
   h1_id	= np.where(final_times[:,3]==0)[0]
   ym1_id	= np.intersect1d(y1_id,m1_id)
   ym2_id	= np.intersect1d(y2_id,m2_id)
@@ -248,3 +248,135 @@ def grab_t2m_region(inputfile,lonlim,latlim,dates):
   #rfe_rg[rfe_rg < 0] = 0
 
   return(t2_rg,match_lon,match_lat)
+
+'''
+2. Grab UKMO GloSea5-GC2 2m temp over a certain region
+'''
+def grab_ukmo_region_t2m(date,inputfile,lonlim,latlim):
+  nc_fid	= Dataset(inputfile,'r')
+  lat		= np.array(nc_fid.variables['latitude'][:] )
+  lon		= np.array(nc_fid.variables['longitude'][:])
+  rfe		= np.array(nc_fid.variables['t2m'][:])
+
+  t_time	= np.array(nc_fid.variables['t'][:])
+  # t_units	= str(nc_fid.variables['t'].units)# these units are wrong!
+  t_units		='hours since '+date+' 00:00:00'
+
+  rfe,lon	= shiftgrid(180.,rfe,lon,start=False)
+  # return datetime objects given numeric time values.
+  all_dates	= nc4.num2date(t_time,t_units)
+
+  # split dates into year, month and day columns
+  final_times	= np.zeros((len(all_dates),3))
+  for t in np.arange(0,len(all_dates)):
+    curr_time		= all_dates[t]
+    final_times[t,0]	= curr_time.year
+    final_times[t,1]	= curr_time.month
+    final_times[t,2]	= curr_time.day
+
+  # grab rfe at specific latitude and longitude (inlat,inlon)
+  # this is interesting: if only condition is given for np.where, return condition.nonzero(), which is a tuple!
+  rg_lon	= np.where( (lon>=lonlim[0]) & (lon<=lonlim[1]) )
+  rg_lat	= np.where( (lat>=latlim[0]) & (lat<=latlim[1]) )
+
+  # [0] here does nothing
+  match_lon	= lon[rg_lon[0]]
+  match_lat	= lat[rg_lat[0]]
+
+  # indexing arrays cannot be broadcast togather
+  # squeeze() here does nothing
+  rfe_rg	= rfe[:,:,rg_lat[0],:][:,:,:,rg_lon[0]].squeeze()
+  nc_fid.close()
+  #rfe_rg[rfe_rg < 0] = 0
+
+  return (rfe_rg,match_lon,match_lat,final_times)
+
+
+
+'''
+3. Grab NCEP t2m over a certain region
+'''
+
+def grab_ncep_region_t2m(date,inputfile,lonlim,latlim):
+  nc_fid	= Dataset(inputfile,'r')
+  lat		= np.array(nc_fid.variables['latitude'][:])
+  lon		= np.array(nc_fid.variables['longitude'][:])
+  rfe		= np.array(nc_fid.variables['t2m'][:])
+
+  t_time	= np.array(nc_fid.variables['t'][:])
+# t_units	= str(nc_fid.variables['t'].units)			# this units is wrong!
+  units		='hours since '+date+' 00:00:00'
+
+  rfe,lon	= shiftgrid(180.,rfe,lon,start=False)
+  # t_time	= nc_fid.variables['time'][:]
+# # convert time to date using netCDF4 function
+  all_dates	= nc4.num2date(t_time,units)				# need to check this works
+  # all_dates = t_time
+
+  # split dates into year, month and day columns
+  final_times	= np.zeros((len(all_dates),4))
+  for t in np.arange(0,len(all_dates)):
+    curr_time		= all_dates[t]
+    final_times[t,0]	= curr_time.year
+    final_times[t,1]	= curr_time.month
+    final_times[t,2]	= curr_time.day
+    final_times[t,3]	= curr_time.hour				# why hour is needed?
+
+  # grab rfe at specific latitude and longitude (inlat,inlon)
+  rg_lon	= np.where( (lon>=lonlim[0]) & (lon<=lonlim[1]) )
+  rg_lat	= np.where( (lat>=latlim[0]) & (lat<=latlim[1]) )
+
+  match_lon	= lon[rg_lon[0]]
+  match_lat	= lat[rg_lat[0]]
+
+  rfe_rg	= rfe[:,:,rg_lat[0],:][:,:,:,rg_lon[0]].squeeze()	# shape is time, lat, lon as shown above
+  nc_fid.close()
+  #rfe_rg[rfe_rg < 0] = 0
+
+  return (rfe_rg,match_lon,match_lat,final_times)
+
+
+'''
+4. Grab ECMWF t2m over a certain region
+
+# e.g. filepath:
+/group_workspaces/jasmin2/klingaman/datasets/S2S/ECMWF/
+ENSO/hindcasts_1120_2017/ecmwf_precip_ens_19981120.nc
+'''
+
+def grab_ecmf_region_t2m(date,inputfile,lonlim,latlim):
+  #print 'loading tamsat data'
+  nc_fid= Dataset(inputfile, 'r')
+  lat	= np.array(nc_fid.variables['latitude'][:])
+  lon	= np.array(nc_fid.variables['longitude'][:])
+  #t_units = str(nc_fid.variables['t'].units) # time units are wrong
+  t_time= np.array(nc_fid.variables['t'][:])
+   # convert time to date using netCDF4 function
+  units='hours since '+date+' 00:00:00'
+  all_dates = nc4.num2date(t_time,units)
+  # all_dates = t_time
+
+  # split dates into year, month and day columns
+  final_times = np.zeros((len(all_dates),4))
+  for t in np.arange(0,len(all_dates)):
+    curr_time = all_dates[t]
+    final_times[t,0] = curr_time.year
+    final_times[t,1] = curr_time.month
+    final_times[t,2] = curr_time.day
+    final_times[t,3] = curr_time.hour
+
+  rfe = np.array(nc_fid.variables['t2m'][:])
+  nc_fid.close()
+
+  rfe,lon = shiftgrid(180.,rfe,lon,start=False)
+
+  # grab rfe at specific latitude and longitude (inlat,inlon)
+  rg_lon = np.where((lon >= lonlim[0]) & (lon <= lonlim[1]))
+  rg_lat = np.where((lat >= latlim[0]) & (lat <= latlim[1]))
+
+  match_lon = lon[rg_lon[0]]
+  match_lat = lat[rg_lat[0]]
+
+  rfe_rg = rfe[:,:,rg_lat[0],:][:,:,:,rg_lon[0]].squeeze()
+
+  return (rfe_rg,match_lon,match_lat,final_times)
